93c7a2cc05cc16d90663642523a263c467983d2b
Gabriel Galdino <gp.galdinodacosta@gmail.com>
2024-02-03 20:01:19 -0300

    Adicionando o conceito de sobrescrita de métodos e anotações.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import br.com.screenmatch.model.Movie;
 import br.com.screenmatch.model.Serie;
+import br.com.screenmatch.model.Titulo;
 
 public class Main {
     public static void main(String[] args) {
         Movie myMovie =  new Movie();
-        myMovie.name = "The Godfather";
-        myMovie.releaseYear = 1970;
-        myMovie.movieTime = 180;
+        myMovie.setName("The Godfather");
+        myMovie.setReleaseYear(1970);
+        myMovie.setMinutesDurationTime(180);
+        System.out.println("Duração do filme: " + myMovie.getMinutesDurationTime());
 
         myMovie.showMovieDescription();
         myMovie.ratingValiation(9);
@@ -14,14 +16,16 @@ public class Main {
         myMovie.ratingValiation((10));
 
         //System.out.println(myMovie.plusRating);
-        System.out.println("Total de avaliações: " + myMovie.getAllReviews());
+        //System.out.println("Total de avaliações: " + myMovie.getAllReviews());
         System.out.println(myMovie.getAvarege());
 
         Serie theOffice = new Serie();
+        theOffice.setReleaseYear(2002);
         theOffice.setTemporadas(9);
         theOffice.setEpisodiosPorTemporada(16);
         theOffice.setMinutosPorEpisodio(23);
-        theOffice.setName("The Office");''
+        theOffice.setName("The Office");
+        System.out.println("Duração para mara maratonar The Office: " + theOffice.getMinutesDurationTime());
 
 
 
--- src/br/com/screenmatch/model/Titulo.java
+++ src/br/com/screenmatch/model/Titulo.java
@@ -7,7 +7,7 @@ public class Titulo {
     private int releaseYear;
     private int allReviews;
     private boolean includedPLan;
-    private int movieTime;
+    private int minutesDurationTime;
 
     int getAllReviews(){
         return allReviews;
@@ -46,18 +46,18 @@ public class Titulo {
         this.includedPLan = includedPLan;
     }
 
-    public int getMovieTime() {
-        return movieTime;
+    public int getMinutesDurationTime() {
+        return minutesDurationTime;
     }
 
-    public void setMovieTime(int movieTime) {
-        this.movieTime = movieTime;
+    public void setMinutesDurationTime(int movieTime) {
+        this.minutesDurationTime = movieTime;
     }
 
     public void showMovieDescription(){
         System.out.println("Nome do filme: " + name + ".");
         System.out.println("Ano de lançamento: " + releaseYear + ".");
-        System.out.println("Tempo de filme em minutos: " + movieTime);
+        System.out.println("Tempo de filme em minutos: " + minutesDurationTime);
     }
 
     public void ratingValiation(double rate){
